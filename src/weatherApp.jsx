d8b15d26c2fe428f50c4fb6589a77871990c340d
Ayushi Pundir <ayushipundir2@gmail.com>
2024-07-18 23:20:36 +0530

    weatherapp commit 3

--- src/weatherApp.jsx
+++ src/weatherApp.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 
 export default function WeatherApp() {
     const [weatherInfo, setWeatherInfo] = useState({
-    p : "Wondering if it's a beach day or a movie day?",
     city : "Delhi",
     feelsLike : 39.05,
     humidity : 74,
